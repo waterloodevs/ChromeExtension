c8c091dac7a334c13789355073a01e4e08f07c59
Jeevan Sidhu <jsidhu@equitable.ca>
2019-03-11 13:14:51 -0400

    added transactions

--- wallet.js
+++ wallet.js
@@ -2,3 +2,5 @@ chrome.storage.local.get(['balance'], function (result) {
     let balance = result['balance'];
     document.getElementById('balance').textContent = balance;
 });
+
+// Get transactions as well
